08b90825046f33546c6b25dd985e1affb9601b7e
MatheusMunizera <matheus.munizera@gmail.com>
2021-09-03 11:46:51 -0300

    New Feature: Add JSON files with builder

---
+++ builder.js
@@ -0,0 +1,86 @@
+
+const fs = require('fs')
+const path = require('path')
+const { promisify } = require('util')
+const writeFile = promisify(fs.writeFile)
+
+
+const characters = require('./api/allCharacters.json')
+const species = require('./api/allSpecies.json')
+const vehicles = require('./api/allVehicles.json')
+const planets = require('./api/allPlanets.json')
+const films = require('./api/allFilms.json')
+const series = require('./api/allSeries.json')
+
+
+const apiFolderPath = 'api'
+
+ const rebuildCharacters = async () => {
+   await Promise.all(characters.map(c => {
+     const filepath = path.join(__dirname, apiFolderPath, 'characters', `${c.id}.json`)
+     return writeFile(filepath, JSON.stringify(c, null, 2))
+   }))
+   .catch(console.error)
+
+   console.log('build characters successful')
+ }
+
+ const rebuildSpecies = async () => {
+   await Promise.all(species.map(c => {
+     const filepath = path.join(__dirname, apiFolderPath, 'species', `${c.id}.json`)
+     return writeFile(filepath, JSON.stringify(c, null, 2))
+   }))
+   .catch(console.error)
+
+   console.log('build species successful')
+ }
+
+ const rebuildVehicles = async () => {
+   await Promise.all(vehicles.map(c => {
+     const filepath = path.join(__dirname, apiFolderPath, 'vehicles', `${c.id}.json`)
+     return writeFile(filepath, JSON.stringify(c, null, 2))
+   }))
+   .catch(console.error)
+
+   console.log('build vehicles successful')
+ }
+
+  const rebuildPlanets = async () => {
+    await Promise.all(planets.map(c => {
+      const filepath = path.join(__dirname, apiFolderPath, 'planets', `${c.id}.json`)
+      return writeFile(filepath, JSON.stringify(c, null, 2))
+    }))
+    .catch(console.error)
+
+    console.log('build planets successful')
+  }
+
+  const rebuildFilms = async () =>{
+    await Promise.all(films.map(c => {
+      const filepath = path.join(__dirname, apiFolderPath, 'films', `${c.id}.json`)
+      return writeFile(filepath, JSON.stringify(c,null,2))
+    }))
+    .catch(console.error)
+
+    console.log('build films successful')
+  }
+
+  const rebuildSeries = async () => {
+    await Promise.all(series.map(c => {
+      const filepath = path.join(__dirname, apiFolderPath, 'series', `${c.id}.json`)
+      return writeFile(filepath, JSON.stringify(c,null,2))
+    }))
+    .catch(console.error)
+    
+    console.log('build series sucessful')
+  }
+
+
+rebuildFilms()
+rebuildCharacters()
+rebuildSpecies()
+rebuildVehicles()
+rebuildPlanets()
+rebuildSeries()
+
+
